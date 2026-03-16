a2901630967e45c423cc759f47b875c679e2c12d
Moncka20 <moncayothomas@gmail.com>
2026-03-15 20:41:29 -0500

    feat: Add RoutesModule and include it in AppModule imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,13 +5,14 @@ import { LoginModule } from './modules/login.module';
 import { DatabaseModule } from './database/database.module';
 import { TypeOrmModule } from '@nestjs/typeorm/dist/typeorm.module';
 import { ConfigModule } from '@nestjs/config/dist/config.module';
+import { RoutesModule } from './routes/routes/routes.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([]),
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
-    }),LoginModule, DatabaseModule],
+    }),LoginModule, DatabaseModule, RoutesModule],
   controllers: [AppController],
   providers: [AppService],
 })
